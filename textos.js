dffcad658dfa4f32d4d59119b50eb0d33827ad41
Enrique Ruiz Durazo <33944811+ruizdurazo@users.noreply.github.com>
2021-11-30 18:38:10 +0100

    Cambio

--- textos.js
+++ textos.js
@@ -3,18 +3,18 @@ const texts = {
   deutsch: {
     //
     meta: {
-      pageTitle: 'This is a test',
+      pageTitle: 'This is a Kizomba',
       pageDescription: 'Descriptive description',
     },
     //
     hero: {
       title: {
-        top: 'Salsa-Tanzschule',
+        top: 'Kizomba-Tanzschule',
         bottom: 'in Zürich',
       },
       intro: [
         // Cada linea es un parrafo:
-        `*BailaRico* ist eine Salsa-Tanzschule in Zürich.`,
+        `*BailaRico* ist eine Kizomba-Tanzschule in Zürich.`,
         `Seit über 10 Jahren bietet BailaRico Kurse
          für *Salsa On2* (NY Mambo), *Salsa On1* (Puerto Rico / Linie), *Bachata*, *Ladies Style*, *Urban* und *High Heels*.`,
       ],
