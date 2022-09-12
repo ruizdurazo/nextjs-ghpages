6fe9ce27a47f42ea4253b8e818c338672fa91df5
Enrique Ruiz Durazo <enrique.ruizdurazo@gmail.com>
2022-09-12 18:08:35 +0200

    Update types

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,24 +1,161 @@
 declare module '*.svg'
 declare module '*.png'
+declare module '*.png?lqip'
 declare module '*.jpeg'
+declare module '*.jpeg?lqip'
 declare module '*.jpg'
+declare module '*.jpg?lqip'
 
-// declare module '*.svg' {
-//   const content: any
-//   export default content
-// }
+// Texts
+interface Texts {
+  de: Translation
+  en: Translation
+  es: Translation
+}
 
-// declare module '*.png' {
-//   const content: any
-//   export default content
-// }
+// Team member
+interface TeamMember {
+  name: string
+  job: string
+  summary: string[]
+}
 
-// declare module '*.jpeg' {
-//   const content: any
-//   export default content
-// }
+// Images
+interface Images {
+  socialShare: string
+  photosAndVideos: string[]
+  socialMedia: string[]
+  studio: {
+    interior: string
+    exterior: string
+  }
+}
 
-// declare module '*.jpg' {
-//   const content: any
-//   export default content
-// }
+interface TranslationWithImages extends Translation {
+  images: Images
+}
+
+// Translation
+interface Translation {
+  // Meta tags
+  meta: {
+    home: {
+      pageTitle: string
+      pageDescription: string
+      // imageSocial: string
+    }
+    error: {
+      pageTitle: string
+      pageDescription: string
+    }
+  }
+  // Navbar menu
+  navbar: {
+    linkCourses: string
+    linkPhotos: string
+    linkContact: string
+    linkHomePage: string
+  }
+  // Hero
+  hero: {
+    title: {
+      top: string
+      middle: string[]
+      bottom: string
+    }
+    intro: string[]
+  }
+  // Courses
+  courses: {
+    // Title
+    title: string
+    // Image
+    courses: string
+    // PDF card
+    pdf: {
+      title: string
+      description: string
+      button: string
+      file: string
+    }
+    // Registration
+    registration: {
+      title: string
+      text: string[]
+    }
+    // Pricing
+    pricing: {
+      title: string
+      text: string[]
+    }
+  }
+  // Photos
+  photos: {
+    // Title
+    title: string
+  }
+  // Team
+  team: {
+    // Title
+    title: string
+    // Team members
+    teamMembers: TeamMember[]
+  }
+  // Social Media
+  socialMedia: {
+    // Title
+    title: string
+    // Text
+    description: string[]
+    // Links
+    linkInstagram: string
+    linkYoutube: string
+    linkFacebook: string
+  }
+  // Contact
+  contact: {
+    // Title
+    title: string
+    // Studio
+    labelStudioExterior: string
+    labelStudioInterior: string
+    // Email
+    labelEmail: string
+    emailAddress: string
+    linkEmail: string
+    // Phone
+    labelPhone: string
+    phoneNumber: string
+    linkPhone: string
+    // Location
+    labelAddress: string
+    address: {
+      streetAndNumber: string
+      postcodeAndCity: string
+    }
+    // Map
+    buttonOpenGoogleMaps: string
+    linkGoogleMaps: string
+  }
+  // Footer
+  footer: {
+    buttonScrollToTop: string
+    copyright: string
+    madeInDesktop: string
+    madeInMobile: string
+    linkToLegal: string
+  }
+  // Error
+  error: {
+    title: string
+    button: string
+  }
+  // Legal
+  legal: {
+    title: string
+    sections: {
+      title: string
+      text: string[]
+    }[]
+  }
+}
