a7bfa546bca39f228f3ecaa40278b69e32264001
Enrique Ruiz Durazo <enrique.ruizdurazo@gmail.com>
2022-09-12 18:08:06 +0200

    Update next config: add custom build, i18n

--- next.config.js
+++ next.config.js
@@ -1,53 +1,41 @@
-/** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-//   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-//   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-// }
+const withPlugins = require('next-compose-plugins') // broken since next 12.2.0
+const optimizedImages = require('next-optimized-images') // broken since next 12.2.0
 
-//
-// const path = require('path')
-// const withOptimizedImages = require('next-optimized-images')
-// module.exports = withOptimizedImages({
-//   /* config for next-optimized-images */
-
-//   images: {
-//     loader: 'custom',
-//     disableStaticImages: true,
-//   },
-
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, 'src/assets/scss')],
-//   },
-
-//   reactStrictMode: true,
-//   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-//   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-// })
-
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-
-module.exports = withPlugins([
+module.exports = withPlugins(
   [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-      optimizeImagesInDev: true,
-      images: {
-        loader: 'custom',
-        disableStaticImages: true,
-      },
-      responsive: {
-        adapter: require('responsive-loader/sharp'),
-      },
+    [
+      optimizedImages,
+      {
+        /* config for next-optimized-images */
+        optimizeImagesInDev: true,
+        images: {
+          loader: 'custom',
+          disableStaticImages: true,
+        },
+        responsive: {
+          adapter: require('responsive-loader/sharp'),
+        },
 
-      // handleImages: ['jpeg', 'png', 'svg'],
-    },
+        handleImages: ['jpeg', 'png', 'jpg'],
+      },
+    ],
   ],
   {
     reactStrictMode: true,
+    future: {
+      // https://nextjs.org/docs/messages/webpack5
+      // webpack5: true, // Causes inconsistent, annoying render error: 'x not found'
+      webpack5: false, // deprecated, next 12+ forces webpack 5
+    },
+    webpack(config) {
+      // Setup reading svgs as components
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+      return config
+    },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  },
-])
+  }
+)
